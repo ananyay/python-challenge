7c38af1fcf1f9257a4ddfb7cda08258b82cd8927
ananya <ananyay.work@gmail.com>
2018-08-02 08:17:31 -0500

    adding text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,26 +1,62 @@
 #importing libraries
 import os 
 import csv 
-# filepath
-filepath = "C:\\Users\\anany\\Desktop\\python_files"
+# setup the file location
+filepath = "C:\\Users\\anany\\repos\\python-challenge\\PyPoll\\Resources"
 csv_filepath = os.path.join(filepath,"election_data.csv")
+# opening the file 
 with open(csv_filepath,newline="",encoding='utf-8') as pypoll:
+    # read the contents of the file 
     csvreader = csv.reader(pypoll,delimiter=",")
+    # skip the header 
     header = next(csvreader)
-    t_votes = []
-    d = {}
-
+    # create an empty lists to store voter id's
+    total_votes = []
+    # create a dictionary with key as candidates , values as voter id 
+    dict_cand_vot = {}
+    # for every row in the file 
     for row in csvreader:
-        t_votes.append(row[0])
-        k = row[2]
-        v = row[0]
-        if k in d:
-            d[k] = d[k]+1
+        total_votes.append(row[0])
+        key = row[2]
+        value = row[0]
+        if key in dict_cand_vot:
+            dict_cand_vot[key] = dict_cand_vot[key]+1
         else:
-            d[k] = 1
-print ("Total Votes:" + str(len(t_votes))) 
-print (d)
+            dict_cand_vot[key] = 1
 
-for key in d:
-    print(key,':',(d[key]/len(t_votes)),(d[key]))
-        
+# print the output
+print("Election Results")
+print("------------------------------")
+
+print ("Total Votes: " + str(len(total_votes))) 
+print("-------------------------------")
+# for each key in the dictionary
+for k in dict_cand_vot:
+    votes_pct = round((dict_cand_vot[k]/len(total_votes)*100.0),3)
+    cand_votes = dict_cand_vot[k]
+    print(k + ": " + str(votes_pct) + "% (" + str(cand_votes) + ")")
+print("-------------------------------")    
+print("winner is : "+ max(dict_cand_vot,key=dict_cand_vot.get))
+print("-------------------------------")
+
+# writing into txt file 
+text_file = open("output_pypoll.txt","w")
+# for writing multiple lines to txt file 
+lines = ["Election Results\n"
+         "------------------------------\n",
+         "Total Votes: " + str(len(total_votes)) + "\n",
+         "-------------------------------\n"]
+text_file.writelines(lines)
+# for each key in the dictionary
+for k in dict_cand_vot:
+    votes_pct = round((dict_cand_vot[k]/len(total_votes)*100.0),3)
+    cand_votes = dict_cand_vot[k]
+    lines = [ k + ": " + str(votes_pct) + "% (" + str(cand_votes) + ")\n"]
+    text_file.writelines(lines)
+#
+lines = ["-------------------------------\n",
+        "winner is : "+ max(dict_cand_vot,key=dict_cand_vot.get)+ "\n"]
+text_file.writelines(lines)
+text_file.close()
+       
+
